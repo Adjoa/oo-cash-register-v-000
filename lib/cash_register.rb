d2482f4c4b02e160c624743395c6088dc58ffe73
Adjoa Darien <adjoa.darien@gmail.com>
2017-12-05 09:51:17 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,29 +1,27 @@
 class CashRegister
   attr_accessor :total, :discount, :title, :price
 
+  ITEMS = {}
+
   def initialize(discount = 0.00)
-    @total = 0.00
-    @discount = discount
+    self.total = 0.00
+    self.discount = discount
   end
 
-  def add_item(title, price, quantity = 1)
-    @total += price * quantity
-  end
-
-  def apply_discount
-    if @discount > 0
-      @total *= (1.00 - discount)
-      puts "#{1.00 - discount}"
-      puts "#{total}"
-    end
-  end
-
-  def items
-
-  end
-
-  def void_last_transaction
-
-  end
+  # def add_item(title, price, quantity = 1)
+  #   @total += price * quantity
+  # end
+  #
+  # def apply_discount
+  #
+  # end
+  #
+  # def items
+  #
+  # end
+  #
+  # def void_last_transaction
+  #
+  # end
 
 end
