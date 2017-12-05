57781082bd9286e8eea66641aa6fc4ae6454a926
Adjoa Darien <adjoa.darien@gmail.com>
2017-12-05 10:04:07 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -15,7 +15,7 @@ class CashRegister
 
   def apply_discount
     self.total *= (1 - self.discount/100)
-    "After the discount, the total comes to $#{self.total}."
+    "After the discount, the total comes to $#{self.total.round}."
   end
   #
   # def items
